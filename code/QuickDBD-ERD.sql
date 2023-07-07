0a105a9c2c079fa208930dc951536df39a601adc
JamesBeckmeyer <jpbeckmeyer@gmail.com>
2023-07-06 19:58:30 -0500

    updated erd syntax

--- code/QuickDBD-ERD.sql
+++ code/QuickDBD-ERD.sql
@@ -3,37 +3,29 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-SET XACT_ABORT ON
+CREATE TABLE "Obesity" (
+    "State" varchar   NOT NULL,
+    "Prevalence" float   NOT NULL,
+    "95%CI" varchar   NOT NULL,
+    CONSTRAINT "pk_Obesity" PRIMARY KEY (
+        "State"
+     )
+);
 
-BEGIN TRANSACTION QUICKDBD
+CREATE TABLE "FastFood" (
+    "State" varchar   NOT NULL,
+    "All_fast_food_restaurants" float   NOT NULL,
+    "Full-service_restaurants" float   NOT NULL,
+    "Subway" float   NOT NULL,
+    "Starbucks" float   NOT NULL,
+    "McDonalds" float   NOT NULL,
+    "Dunkin_Donut" float   NOT NULL,
+    "Burger_King" float   NOT NULL,
+    "Taco_Bell" float   NOT NULL,
+    "Dominos" float   NOT NULL,
+    "Wendys" float   NOT NULL,
+    "Dairy_Queen" float   NOT NULL
+);
 
-CREATE TABLE [Obesity] (
-    [State] varchar  NOT NULL ,
-    [Prevalence] float  NOT NULL ,
-    [95%CI] float  NOT NULL ,
-    CONSTRAINT [PK_Obesity] PRIMARY KEY CLUSTERED (
-        [State] ASC
-    )
-)
-
-CREATE TABLE [FastFood] (
-    [State] varchar  NOT NULL ,
-    [All_fast_food_restaurants] float  NOT NULL ,
-    [Full-service_restaurants] float  NOT NULL ,
-    [Subway] float  NOT NULL ,
-    [Starbucks] float  NOT NULL ,
-    [McDonalds] float  NOT NULL ,
-    [Dunkin_Donut] float  NOT NULL ,
-    [Burger_King] float  NOT NULL ,
-    [Taco_Bell] float  NOT NULL ,
-    [Dominos] float  NOT NULL ,
-    [Wendys] float  NOT NULL ,
-    [Dairy_Queen] float  NOT NULL 
-)
-
-ALTER TABLE [FastFood] WITH CHECK ADD CONSTRAINT [FK_FastFood_State] FOREIGN KEY([State])
-REFERENCES [Obesity] ([State])
-
-ALTER TABLE [FastFood] CHECK CONSTRAINT [FK_FastFood_State]
-
-COMMIT TRANSACTION QUICKDBD
+ALTER TABLE "FastFood" ADD CONSTRAINT "fk_FastFood_State" FOREIGN KEY("State")
+REFERENCES "Obesity" ("State");
