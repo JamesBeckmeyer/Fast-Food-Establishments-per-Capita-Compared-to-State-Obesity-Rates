497b586fcc6b4dd2a91f436ae718675305830662
AkagiNaoko <rji.edu.code@gmail.com>
2023-07-06 20:29:50 -0500

    testing ERD

--- code/QuickDBD-ERD.sql
+++ code/QuickDBD-ERD.sql
@@ -3,16 +3,16 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Obesity" (
+CREATE TABLE Obesity (
     "State" varchar   NOT NULL,
-    "Prevalence" float   NOT NULL,
+    "Prevalence" varchar   NOT NULL,
     "95%CI" varchar   NOT NULL,
     CONSTRAINT "pk_Obesity" PRIMARY KEY (
         "State"
      )
 );
 
-CREATE TABLE "FastFood" (
+CREATE TABLE FastFood (
     "State" varchar   NOT NULL,
     "All_fast_food_restaurants" float   NOT NULL,
     "Full-service_restaurants" float   NOT NULL,
@@ -27,5 +27,6 @@ CREATE TABLE "FastFood" (
     "Dairy_Queen" float   NOT NULL
 );
 
-ALTER TABLE "FastFood" ADD CONSTRAINT "fk_FastFood_State" FOREIGN KEY("State")
-REFERENCES "Obesity" ("State");
+ALTER TABLE FastFood ADD CONSTRAINT "fk_FastFood_State" FOREIGN KEY("State")
+REFERENCES Obesity ("State");
+
